8e9ca34581f7816605eaa5bac16b955efea8cc22
parvathi1212 <98682771+parvathi1212@users.noreply.github.com>
2022-10-06 12:50:31 +0530

    added login router

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const app = express()
+const request = reuire('request');
 var router = express.Router();
 var path = require('path');
 
@@ -6,5 +8,9 @@ var path = require('path');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/loginpage',function(req,res){
+  res.sendFile(path.resolve('public/loginpage.html'))
+})
 
 module.exports = router;
